3c6f7624adfc1131769fabbbe14d610baea44fb2
luanshixia <dreambuild@qq.com>
2015-03-10 09:40:09 +0800

    Update EasyLoad: adding TheRightWay

---
+++ EasyLoad/EasyLoad/TheRightWay/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;
+
+namespace EasyLoad.TheRightWay
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Current { get; private set; }
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Current = this;
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            try
+            {
+                await TestManager.BeginTest(RequestUrlBox.Text, Convert.ToInt32(RequestCountBox.Text), Convert.ToInt32(RequestGapBox.Text), _cts.Token);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "EasyLoad", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void TheList_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            if (TheList.SelectedItem != null)
+            {
+                ResponseDetailWindow rdw = new ResponseDetailWindow { Owner = this };
+                rdw.TheTextBox.Text = (TheList.SelectedItem as TestRecord).Response;
+                rdw.ShowDialog();
+            }
+        }
+
+        private void CancelButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            _cts.Cancel();
+        }
+    }
+}
---
+++ EasyLoad/EasyLoad/TheRightWay/TestManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Threading;
+
+namespace EasyLoad.TheRightWay
+{
+    public class TestRecord
+    {
+        public int Number { get; set; }
+        public string SendTime { get; set; }
+        public string ReceiveTime { get; set; }
+        public double Delay { get; set; }
+        public string Response { get; set; }
+        public DateTime RawSendTime { get; set; }
+    }
+
+    public static class TestManager
+    {
+        public static async Task BeginTest(string url, int times, int gap, CancellationToken ct)
+        {
+            MainWindow.Current.TheList.Items.Clear();
+            await Task.WhenAll(Enumerable.Range(0, times)
+                .Select(i => DoSingleRequest(url, i, gap))
+                .ToArray());
+        }
+
+        public static async Task DoSingleRequest(string url, int i, int gap)
+        {
+            using (WebClient client = new WebClient { Encoding = System.Text.Encoding.Default })
+            {
+                await Task.Delay(i * gap);
+
+                TestRecord record = new TestRecord();
+                var sendTime = DateTime.Now;
+                record.Number = i + 1;
+                record.SendTime = FormatTime(sendTime);
+                MainWindow.Current.TheList.Items.Add(record);
+
+                string response = await client.DownloadStringTaskAsync(url);
+                var receiveTime = DateTime.Now;
+                record.ReceiveTime = FormatTime(receiveTime);
+                record.Delay = (receiveTime - sendTime).TotalMilliseconds;
+                record.Response = response;
+                MainWindow.Current.TheList.Items.Refresh();
+            }
+        }
+
+        private static string FormatTime(DateTime time)
+        {
+            return string.Format("{0}:{1}:{2}:{3:000}", time.Hour, time.Minute, time.Second, time.Millisecond);
+        }
+    }
+}
