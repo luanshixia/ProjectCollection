17039e35322e45a37a12682741f8ffd73d172ebc
luanshixia <dreambuild@qq.com>
2015-03-27 11:06:37 +0800

    Add MyAsync project

---
+++ MyAsync/MyAsync/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // This case shows a long CPU task will block the UI just as sync op...
+        private async void ButtonLongRun_Click(object sender, RoutedEventArgs e)
+        {
+            long result = await Task.FromResult(Fib(40));
+            MessageBox.Show(result.ToString());
+        }
+
+        // ...unless using Task.Run() to do it in a new thread
+        private async void ButtonLongRun2_Click(object sender, RoutedEventArgs e)
+        {
+            long result = await Task.Run(() => Fib(40));
+            MessageBox.Show(result.ToString());
+        }
+
+        private static long Fib(long n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+
+        // This case shows the executing order with await.
+        private async void ButtonLoopAwait_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Label1.Content = i;
+                await Task.Delay(500);
+            }
+            Label1.Content = "done.";
+        }
+    }
+}
