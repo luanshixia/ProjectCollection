0aff4a0eb6929bccc38bd54abaac0503582971df
luanshixia <dreambuild@qq.com>
2015-07-15 13:15:44 +0800

    Support Chinese

--- HtmlToImage/HtmlToImage/ImageRenderer.cs
+++ HtmlToImage/HtmlToImage/ImageRenderer.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.PhantomJS;
@@ -19,12 +20,24 @@ namespace HtmlToImage
             {
                 //string dataUri = string.Format("data:text/html;base64,{0}", 
                 //    Convert.ToBase64String(new UnicodeEncoding().GetBytes(html)));
-                string dataUri = string.Format("data:text/html,{0}", html);
+                string dataUri = string.Format("data:text/html;charset=utf-8,{0}", EncodeNoneAscii(html));
                 driver.Navigate().GoToUrl(dataUri);
                 var screenshot = driver.TakeScreenshot();
                 var outBytes = screenshot.AsByteArray;
                 output.Write(outBytes, 0, outBytes.Length);
             }
+        }
+
+        private string EncodeNoneAscii(string text)
+        {
+            return string.Join(null, text.Select(c =>
+            {
+                if (c > 127)
+                {
+                    return HttpUtility.UrlEncode(c.ToString());
+                }
+                return c.ToString();
+            }).ToArray());
         }
     }
 }
