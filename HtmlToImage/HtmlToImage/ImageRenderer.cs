aba0a57edc9ae3f5f45f99918d00021085fe932c
luanshixia <dreambuild@qq.com>
2015-07-15 11:40:37 +0800

    Add project HtmlToImage

---
+++ HtmlToImage/HtmlToImage/ImageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.Extensions;
+
+namespace HtmlToImage
+{
+    public class ImageRenderer
+    {
+        public void ExportAsync(string html, Stream output)
+        {
+            using (var driver = new PhantomJSDriver())
+            {
+                //string dataUri = string.Format("data:text/html;base64,{0}", 
+                //    Convert.ToBase64String(new UnicodeEncoding().GetBytes(html)));
+                string dataUri = string.Format("data:text/html,{0}", html);
+                driver.Navigate().GoToUrl(dataUri);
+                var screenshot = driver.TakeScreenshot();
+                var outBytes = screenshot.AsByteArray;
+                output.Write(outBytes, 0, outBytes.Length);
+            }
+        }
+    }
+}
---
+++ HtmlToImage/HtmlToImage/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HtmlToImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_New_Click(object sender, RoutedEventArgs e)
+        {
+            TextArea.Text = string.Empty;
+        }
+
+        private void MenuItem_Show_Click(object sender, RoutedEventArgs e)
+        {
+            using (var stream = new System.IO.MemoryStream())
+            {
+                var renderer = new ImageRenderer();
+                renderer.ExportAsync(TextArea.Text, stream);
+                var bf = BitmapFrame.Create(stream, 
+                    BitmapCreateOptions.None, 
+                    BitmapCacheOption.OnLoad);
+                Image image = new Image { Source = bf, Width = bf.PixelWidth, Height = bf.PixelHeight };
+                ScrollViewer sv = new ScrollViewer { Content = image, HorizontalScrollBarVisibility = ScrollBarVisibility.Auto };
+                Window window = new Window { Content = sv, Title = "HTML Preview", 
+                    Owner = this, WindowStyle = WindowStyle.ToolWindow };
+                window.ShowDialog();
+            }
+        }
+    }
+}
