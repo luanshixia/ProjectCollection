0f316701970b07b2f5791038522e6f5a31238a80
Yang Wang <luanshixia@gmail.com>
2018-06-28 22:59:11 -0700

    Refactor CsvExporter

--- Dreambuild.Common/Dreambuild.Common/IO/CsvExporter.cs
+++ Dreambuild.Common/Dreambuild.Common/IO/CsvExporter.cs
@@ -1,64 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using Dreambuild.Extensions;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Dreambuild.IO
 {
     /// <summary>
-    /// CSV file creation
+    /// CSV file creation.
     /// </summary>
-    /// <typeparam name="T">Type of data record.</typeparam>
+    /// <typeparam name="T">The type of data record.</typeparam>
     public class CsvExporter<T>
     {
-        public List<T> Data { get; private set; }
-        public List<string> PropertyPaths { get; private set; }
-        public List<string> Headers { get; private set; }
+        public List<T> Data { get; }
+        public List<string> PropertyPaths { get; }
+        public List<string> Headers { get; }
         public bool ShowHeaders { get; set; }
 
         public CsvExporter(List<T> data, List<string> propertyPaths)
         {
-            Data = data;
-            PropertyPaths = propertyPaths;
-            Headers = new List<string>();
-            ShowHeaders = false;
+            this.Data = data;
+            this.PropertyPaths = propertyPaths;
+            this.Headers = new List<string>();
+            this.ShowHeaders = false;
         }
 
         public void Export(string fileName)
         {
-            var lines = GetLines();
-            System.IO.File.WriteAllLines(fileName, lines.ToArray());
+            var lines = this.GetLines();
+            File.WriteAllLines(fileName, lines.ToArray());
         }
 
         private List<string> GetLines()
         {
-            var lines = Data.Select(x => GetLine(x)).ToList();
-            if (ShowHeaders)
+            var lines = this.Data
+                .Select(x => this.GetLine(x))
+                .ToList();
+
+            if (this.ShowHeaders)
             {
-                lines.Insert(0, GetHeaderLine());
+                lines.Insert(0, this.GetHeaderLine());
             }
+
             return lines;
         }
 
         private string GetLine(T item)
         {
-            return string.Join(",", PropertyPaths.Select(x => GetPropertyValue(item, x).ToString()).ToArray());
+            return string.Join(
+                separator: ",",
+                value: this.PropertyPaths
+                    .Select(path => item.GetPropertyValue(path).ToString())
+                    .ToArray());
         }
 
         private string GetHeaderLine()
         {
-            return string.Join(",", Headers.ToArray());
-        }
-
-        private static object GetPropertyValue(object obj, string path) // mod 20170303
-        {
-            var paths = path.Split('.');
-            var host = obj;
-            foreach (var prop in paths)
-            {
-                host = host.GetType().GetRuntimeProperty(prop).GetValue(host);
-            }
-            return host;
+            return string.Join(",", this.Headers.ToArray());
         }
     }
 }
