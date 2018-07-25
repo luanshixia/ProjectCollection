f55f7b472be278c61315c78d6b05289bc55e52cc
Yang Wang <luanshixia@gmail.com>
2018-07-25 15:19:14 -0700

    Use class

--- jqShim/src/index.js
+++ jqShim/src/index.js
@@ -2,37 +2,45 @@
 
 const version = '0.1.0';
 
-function jqShim(selector, context) {
-  return new jqShim.fn.init(selector, context);
-}
+class jqShim {
 
-jqShim.fn = jqShim.prototype = {
+  static extend(...args) {
+    if (args.length === 1) {
+      return Object.assign(jqShim, args[0]);
+    }
 
-  jquery: version,
-
-  constructor: jqShim,
-
-  length: 0,
-
-  each(callback) {
-    return jqShim.each(this, callback);
-  }
-};
-
-jqShim.extend = jqShim.fn.extend = function extend(...args) {
-  if (args.length === 1) {
-    return extend(this, args[0]);
+    return Object.assign(...args);
   }
 
-  return Object.assign(...args);
-};
-
-jqShim.extend({
-  each(obj, callback) {
+  static each(obj, callback) {
     Array.from(obj, callback);
-  },
+  }
 
-  map(elems, callback) {
+  static map(elems, callback) {
     return Array.from(elems, callback);
   }
-});
+
+  get jquery() {
+    return version;
+  }
+
+  get length() {
+    return this.elems.length;
+  }
+
+  constructor(selector) {
+    this.elems = [];
+  }
+
+  extend(...args) {
+    if (args.length === 1) {
+      return Object.assign(this, args[0]);
+    }
+
+    return Object.assign(...args);
+  }
+
+  each(callback) {
+    Array.from(this, callback);
+  }
+}
