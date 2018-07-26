4941a5f66671ece4c7444216657aed5d24c91a11
Yang Wang <luanshixia@gmail.com>
2018-07-26 10:19:36 -0700

    Fix test

--- jqShim/test/test.html.js
+++ jqShim/test/test.html.js
@@ -14,6 +14,20 @@ describe('API tests', function() {
       const jq = $('<div>');
       assert.equal(1, jq.elems.length);
     });
+
+    it('can select tags by selector', function() {
+      const jq = $('#mocha');
+      assert.equal(1, jq.elems.length);
+
+      const jq1 = $('head script');
+      assert.equal(2, jq1.elems.length);
+    });
+
+    it('can wrap tags', function() {
+    });
+
+    it('can wrap single tag', function() {
+    });
   });
 });
 
