d2fa612e5be093a96c1ba46a7af6c92979a1525a
Yang Wang <luanshixia@gmail.com>
2018-07-30 19:08:56 -0700

    Add pan() zoom()

--- eMind/public/scripts/viewer.js
+++ eMind/public/scripts/viewer.js
@@ -5,12 +5,38 @@ class Viewer extends Widget {
   constructor($element, options) {
     super($element, options);
 
-    this.center = [0, 0];
+    this.$content = $element.find('dreambuild-viewer-content');
+    this.origin = { x: 0, y: 0 };
     this.scale = 1;
   }
 
   _create() {
-    this.$element.html(this.options.text);
+    this._init();
+  }
+
+  _init() {
+    this.$element.addClass('dreambuild-viewer dreambuild-viewer-border');
+  }
+
+  pan(displacement) {
+    this.origin.x += displacement.x;
+    this.origin.y += displacement.y;
+    this._render();
+  }
+
+  zoom(newScale, basePoint) {
+    const v1x = basePoint.x - this.origin.x;
+    const v1y = basePoint.y - this.origin.y;
+    const v2x = (this.scale / newScale) * v1x;
+    const v2y = (this.scale / newScale) * v1y;
+    this.scale = newScale;
+    this.origin.x += v1x - v2x;
+    this.origin.y += v1y - v2y;
+    this._render();
+  }
+
+  _render() {
+    this.$content.css('transform', null);
   }
 }
 
