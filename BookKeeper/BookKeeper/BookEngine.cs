a64e8bd48ea709ca12d9f6d0ffa6a25bb5a40989
Yang Wang <luanshixia@gmail.com>
2018-09-08 21:40:58 -0700

    XQuery draft

--- BookKeeper/BookKeeper/BookEngine.cs
+++ BookKeeper/BookKeeper/BookEngine.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Xml.Linq;
 
 namespace BookKeeper
 {
     public class BookEngine
     {
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
+
+        public const string Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+
+        public const string AcceptEncoding = "gzip, deflate, br";
+
+        public const string AcceptLanguage = "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7";
+
+        public const string UriTemplate = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dstripbooks&field-keywords={0}";
+
+        public static async void Search(string kwd)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(BookEngine.UserAgent);
+                httpClient.DefaultRequestHeaders.Accept.ParseAdd(BookEngine.Accept);
+                httpClient.DefaultRequestHeaders.AcceptEncoding.ParseAdd(BookEngine.AcceptEncoding);
+                httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd(BookEngine.AcceptLanguage);
+
+                var requestUri = string.Format(BookEngine.UriTemplate, kwd);
+                var response = await httpClient.GetStringAsync(requestUri);
+
+                var xd = XDocument.Parse(response);
+            }
+        }
     }
 }
---
+++ BookKeeper/BookKeeper/XQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BookKeeper
+{
+    public class XQuery
+    {
+        public XElement Element { get; }
+
+        private XQuery(XElement element)
+        {
+            this.Element = element;
+        }
+
+        public static XQuery Create(XElement element)
+        {
+            return new XQuery(element);
+        }
+
+        public XQuery Find()
+        {
+
+        }
+    }
+}
