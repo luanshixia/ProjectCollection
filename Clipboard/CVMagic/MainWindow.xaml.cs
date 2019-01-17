036d786101c17a8dcf1b0b3b249662d70a36aedc
Yang Wang <luanshixia@gmail.com>
2019-01-17 12:13:04 -0800

    Add project CVMagic

---
+++ Clipboard/CVMagic/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CVMagic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.SourceInitialized += (sender, e) =>
+            {
+                var clipboardManager = new ClipboardManager(this);
+                clipboardManager.ClipboardChanged += (sender1, e1) =>
+                {
+                    if (Clipboard.ContainsText())
+                    {
+                        var text = Clipboard.GetText();
+                        if (text.StartsWith("ago"))
+                        {
+                            Clipboard.SetText("bingo!");
+                        }
+                    }
+                };
+            };
+        }
+    }
+}
