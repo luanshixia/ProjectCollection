b21197e6262edb162a8b7728d112af8f63c50f03
Yang Wang <luanshixia@gmail.com>
2019-01-24 18:21:56 -0800

    Try Regex groups

--- Clipboard/CVMagic/MainWindow.xaml.cs
+++ Clipboard/CVMagic/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,6 +36,16 @@ namespace CVMagic
                         if (text.StartsWith("ago"))
                         {
                             Clipboard.SetText("bingo!");
+                        }
+                        else if (Regex.Match(text, @"TIMESTAMP\s*>\s*ago\((?<point>[0-9]+[dhms])\)", RegexOptions.IgnoreCase) is Match match && match.Success)
+                        {
+                            var point = match.Groups["point"].Value;
+                            if (Regex.Match(point, "(?<days>[0-9]+)d") is Match dayMatch && dayMatch.Success)
+                            {
+                                var days = dayMatch.Groups["days"].Value;
+                                var datetime = DateTime.UtcNow.AddDays(-int.Parse(days));
+                                Clipboard.SetText($"TIMESTAMP > datetime({datetime})");
+                            }
                         }
                     }
                 };
