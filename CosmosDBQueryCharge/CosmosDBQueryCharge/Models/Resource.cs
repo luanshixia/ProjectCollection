ff3773742eb824b3a0790e9335994a500961d4ba
Yang Wang <luanshixia@gmail.com>
2019-04-02 12:31:39 -0700

    Generate db

--- CosmosDBQueryCharge/CosmosDBQueryCharge/Models/Resource.cs
+++ CosmosDBQueryCharge/CosmosDBQueryCharge/Models/Resource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -94,7 +90,7 @@ namespace CosmosDBQueryCharge
         {
             get
             {
-                return $"{this.ResourceGroupName}-{this.ResourceId}-{this.Location}";
+                return $"{this.ResourceGroupName}-{EscapeStorageKey(this.ResourceId)}-{this.Location}";
             }
         }
 
@@ -108,6 +104,55 @@ namespace CosmosDBQueryCharge
             {
                 return this.SubscriptionId;
             }
+        }
+
+        /// <summary>
+        /// The escaped storage keys.
+        /// </summary>
+        private static readonly string[] EscapedStorageKeys = new string[128]
+        {
+            ":00", ":01", ":02", ":03", ":04", ":05", ":06", ":07", ":08", ":09", ":0A", ":0B", ":0C", ":0D", ":0E", ":0F",
+            ":10", ":11", ":12", ":13", ":14", ":15", ":16", ":17", ":18", ":19", ":1A", ":1B", ":1C", ":1D", ":1E", ":1F",
+            ":20", ":21", ":22", ":23", ":24", ":25", ":26", ":27", ":28", ":29", ":2A", ":2B", ":2C", ":2D", ":2E", ":2F",
+            "0",   "1",   "2",   "3",   "4",   "5",   "6",   "7",   "8",   "9", ":3A", ":3B", ":3C", ":3D", ":3E", ":3F",
+            ":40",   "A",   "B",   "C",   "D",   "E",   "F",   "G",   "H",   "I",   "J",   "K",   "L",   "M",   "N",   "O",
+            "P",   "Q",   "R",   "S",   "T",   "U",   "V",   "W",   "X",   "Y",   "Z", ":5B", ":5C", ":5D", ":5E", ":5F",
+            ":60",   "a",   "b",   "c",   "d",   "e",   "f",   "g",   "h",   "i",   "j",   "k",   "l",   "m",   "n",   "o",
+            "p",   "q",   "r",   "s",   "t",   "u",   "v",   "w",   "x",   "y",   "z", ":7B", ":7C", ":7D", ":7E", ":7F",
+        };
+
+        /// <summary>
+        /// Escapes the storage key.
+        /// </summary>
+        /// <param name="storageKey">The storage key.</param>
+        public static string EscapeStorageKey(string storageKey)
+        {
+            var sb = new StringBuilder(storageKey.Length);
+            for (var index = 0; index < storageKey.Length; ++index)
+            {
+                var c = storageKey[index];
+                if (c < 128)
+                {
+                    sb.Append(EscapedStorageKeys[c]);
+                }
+                else if (char.IsLetterOrDigit(c))
+                {
+                    sb.Append(c);
+                }
+                else if (c < 0x100)
+                {
+                    sb.Append(':');
+                    sb.Append(((int)c).ToString("X2"));
+                }
+                else
+                {
+                    sb.Append(':');
+                    sb.Append(':');
+                    sb.Append(((int)c).ToString("X4"));
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
--- CosmosDBQueryCharge/CosmosDBQueryCharge/Program.cs
+++ CosmosDBQueryCharge/CosmosDBQueryCharge/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
+using System.Security;
 using System.Text;
+using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
 
 namespace CosmosDBQueryCharge
@@ -14,8 +16,19 @@ namespace CosmosDBQueryCharge
             new[] { 1 },
         };
 
+        private static readonly int[][] DbDataGenerationPatterns =
+        {
+            new[] { 3, 4, 5 },
+            new[] { 2 },
+            new[] { 1 },
+        };
+
         static void Main(string[] args)
         {
+            var documentClient = new DocumentClient(
+                serviceEndpoint: new Uri("https://arm-cits-globaldata.documents.azure.com:443/"),
+                authKey: GetAuthKey());
+
             if (args.First().Equals("generate"))
             {
                 var resourceGenerator = new ResourceGenerator(DataGenerationPatterns);
@@ -29,8 +42,36 @@ namespace CosmosDBQueryCharge
                     Console.WriteLine(Encoding.UTF8.GetBytes(json).Length);
                 }
             }
+            else if (args.First().Equals("dbgenerate"))
+            {
+                var resourceGenerator = new ResourceGenerator(DbDataGenerationPatterns);
+                foreach (var resource in resourceGenerator.GenerateAll())
+                {
+                    var response = documentClient
+                        .CreateDocumentAsync(
+                            documentCollectionUri: UriFactory.CreateDocumentCollectionUri(
+                                databaseId: "ARMLocalData",
+                                collectionId: "resources"),
+                            document: resource)
+                        .Result;
+
+                    Console.WriteLine($"Size={Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(resource)).Length}|Charge={response.RequestCharge}");
+                }
+            }
 
             Console.ReadKey();
+        }
+
+        private static SecureString GetAuthKey()
+        {
+            var encodedKey = "";
+            var secureString = new SecureString();
+            foreach (var c in Encoding.UTF8.GetString(Convert.FromBase64String(encodedKey)))
+            {
+                secureString.AppendChar(c);
+            }
+
+            return secureString;
         }
     }
 }
