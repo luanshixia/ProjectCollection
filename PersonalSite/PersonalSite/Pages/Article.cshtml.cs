4b87e11d4b0dc80535f1b918f270ec0d0bfb5919
Yang Wang <luanshixia@gmail.com>
2019-05-22 18:20:26 -0700

    Add article page

---
+++ PersonalSite/PersonalSite/Pages/Article.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalSite.Pages
+{
+    public class ArticleModel : PageModel
+    {
+        private IHostingEnvironment _env;
+
+        public string Name { get; set; }
+
+        public string Markdown { get; set; }
+
+        public string Html { get; set; }
+
+        public ArticleModel(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Name = RouteData.Values["name"].ToString();
+            Markdown = await System.IO.File.ReadAllTextAsync(System.IO.Path.Combine(_env.ContentRootPath, "Articles", $"{Name}.md"));
+            Html = Markdig.Markdown.ToHtml(Markdown);
+        }
+    }
+}
