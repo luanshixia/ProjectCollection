ce4e1f6a73813a836d644b6ab8e6703efec9fa87
Yang Wang <luanshixia@gmail.com>
2025-02-06 21:28:58 -0800

    Add project StashCam

---
+++ StashCam/StashCam/StashCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashCamApp.swift
+//  StashCam
+//
+//  Created by Yang Wang on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StashCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StashCam/StashCamTests/StashCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  StashCamTests.swift
+//  StashCamTests
+//
+//  Created by Yang Wang on 2/6/25.
+//
+
+import Testing
+@testable import StashCam
+
+struct StashCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
